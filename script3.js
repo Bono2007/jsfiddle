30ecdd3efb08207094026a0efa661d38c07cf83f
Bono2007 <llc314159@gmail.com>
2023-06-12 22:49:33 +0200

    Rassemblement et séparation ok -collisions -zone clic

--- script3.js
+++ script3.js
@@ -5,7 +5,7 @@ const config = {
     height: 400,
     physics: {
         default: "arcade",
-        debug: true
+        debug: false
     },
     scene: {
         preload: preload,
@@ -20,8 +20,7 @@ let gameScene = {
     acontGameBoys: [],
     acontFishes: [],
     clickedFirst: "",
-    clickedSecond: "",
-    tweenInProgress:false
+    clickedSecond: ""
 };
 
 function preload() {
@@ -65,132 +64,129 @@ function createGameObject(imageKey, text, position, containerArray, type) {
     image.on("pointerdown", () => {
         handlePointerDown.call(this, container);
     });
-    
 }
 
 function handlePointerDown(container) {
-    if (gameScene.tweenInProgress) {
-        return;
-    }
     // Si le conteneur cliqué contient déjà un autre conteneur
     if (container.list.length > 2) {
+        console.log("1:")
         // On récupère le conteneur enfant
         let childContainer = container.list[2];
         // On retire le conteneur enfant du conteneur parent
         container.remove(childContainer);
+        console.log("2:")
         // On ajoute le conteneur enfant à la scène
         this.add.existing(childContainer);
-        console.log(childContainer.type)
-        if (childContainer.type=="gameboy") {
-            childContainer.x = container.x - container.list[0].width;
-        } else {
-            childContainer.x = container.x + container.list[0].width;
-        }
+        console.log("3:")
+        childContainer.x=container.x;
         childContainer.y=container.y;
+        console.log("4:")
+        this.tweens.add({
+            targets: childContainer,
+            x:735/2,
+            y:400/2,
+            duration: 1000
+        })
+        console.log("5:")
         // On réactive la physique pour le conteneur enfant
         this.physics.world.enable(childContainer);
+        console.log("6:")
         childContainer.body.setVelocity(Phaser.Math.Between(-200, 200), Phaser.Math.Between(-150, 150));
         // On réinitialise firstClickedContainer et clickedFirst
-        gameScene.firstClickedContainer = null;
+        //gameScene.firstClickedContainer = null;
         gameScene.clickedFirst = "";
-        gameScene.secondClickedContainer = null;
+        //gameScene.secondClickedContainer = null;
         gameScene.clickedSecond = "";
+        console.log("7:")
     } else
     if (container.type === "gameboy") {
+        console.log("11:")
         if (gameScene.clickedFirst === "fish") {
+            console.log("12:")
             // Si un fish a déjà été cliqué
-            // On évite le clic rapide sur plusieurs fish qui mettrait plusieurs containers
-
             gameScene.clickedSecond = "gameboy";
             gameScene.secondClickedContainer = container;
             // On enregistre la position actuelle du fish
             let targetX = gameScene.firstClickedContainer.x-gameScene.firstClickedContainer.list[0].width;
             let targetY = gameScene.firstClickedContainer.y;
+            console.log("13:")
             // On crée un tween pour déplacer le fish vers le gameboy
             let tween=this.tweens.add({
                 targets: container,
                 x: targetX,
                 y: targetY,
                 duration: 1000,
-                onStart: () => {
-                   // Pour éviter de mettre plusieurs containers dans le 1e cliqué
-                    gameScene.tweenInProgress = true;
-                },
                 onUpdate: () => {
                     // Met à jour la position cible du tween pour suivre le gameboy en mouvement
                     tween.updateTo('x', gameScene.firstClickedContainer.x-gameScene.firstClickedContainer.list[0].width, true);
                     tween.updateTo('y', gameScene.firstClickedContainer.y, true);
                 },
                 onComplete: () => {
-                    gameScene.tweenInProgress = false;
                     // On désactive la physique pour le gameboy
                     container.body.stop();
                     container.body.enable = false;
-
+                    console.log("14:")
                     // On ajoute le gameboy au conteneur du fish
                     gameScene.firstClickedContainer.add(container);
-
+                    console.log("15:")
                     // On ajuste la position du gameboy à l'intérieur du conteneur du fish
                     container.x = -gameScene.firstClickedContainer.list[0].width;
                     container.y = 0;
-
-                    //
-                    gameScene.firstClickedContainer.setInteractive(new Phaser.Geom.Rectangle(0, 0, gameScene.firstClickedContainer.width * 2, gameScene.firstClickedContainer.height), Phaser.Geom.Rectangle.Contains);
-
+                    console.log("16:")
                     // On réinitialise les clics
                     gameScene.clickedFirst = "";
                     gameScene.clickedSecond = "";
+                    console.log("17:")
                 }
             });
         } else if (gameScene.clickedFirst === "") {
+            console.log("21:")
             // Si rien n'a été cliqué auparavant, on enregistre le gameboy cliqué
             gameScene.clickedFirst = "gameboy";
             gameScene.firstClickedContainer = container;
         }
     } else if (container.type === "fish") {
+        console.log("31:")
         if (gameScene.clickedFirst === "gameboy") {
             // Si un gameboy a déjà été cliqué
             gameScene.clickedSecond = "fish";
             gameScene.secondClickedContainer = container;
-
+            console.log("32:")
             // On enregistre la position actuelle du gameboy
             let targetX = gameScene.firstClickedContainer.x;
             let targetY = gameScene.firstClickedContainer.y;
-            
+            console.log("33:")
             // On crée un tween pour déplacer le gameboy vers le fish
             let tween=this.tweens.add({
                 targets: container,
                 x: targetX,
                 y: targetY,
                 duration: 1000,
-                onStart: () => {
-                    // Pour éviter de mettre plusieurs containers dans le 1e cliqué
-                     gameScene.tweenInProgress = true;
-                 },
                 onUpdate: () => {
                     // Met à jour la position cible du tween pour suivre le gameboy en mouvement
                     tween.updateTo('x', gameScene.firstClickedContainer.x+gameScene.firstClickedContainer.list[0].width, true);
                     tween.updateTo('y', gameScene.firstClickedContainer.y, true);
+                    console.log("34:")
                 },
                 onComplete: () => {
-                    gameScene.tweenInProgress = false;
                     // On désactive la physique pour le fish
                     container.body.stop();
                     container.body.enable = false;
-
+                    console.log("35:")
                     // On ajoute le fish au conteneur du gameboy
                     gameScene.firstClickedContainer.add(container);
-
+                    console.log("36:")
                     // On ajuste la position du fish à l'intérieur du conteneur du gameboy
                     container.x = gameScene.firstClickedContainer.list[0].width;
                     container.y = 0;
-
+                    console.log("37:"+gameScene.firstClickedContainer.list[0].width)
                     // On réinitialise les clics
                     gameScene.clickedFirst = "";
                     gameScene.clickedSecond = "";
                 }
             });
         } else if (gameScene.clickedFirst === "") {
+            console.log("41:")
             // Si rien n'a été cliqué auparavant, on enregistre le fish cliqué
             gameScene.clickedFirst = "fish";
             gameScene.firstClickedContainer = container;
